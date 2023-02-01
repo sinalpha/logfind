95fc4f4294bbddc2d0de387a5bbc226ef505481f
Sinalpha <59336397+sinalpha@users.noreply.github.com>
2023-02-01 19:23:23 +0900

    Update findfile.h

--- lib/findfile.h
+++ lib/findfile.h
@@ -8,11 +8,11 @@
 char **findFile(int option, char** keywordList, int keywordListC, char** fileNameList, int fileNameListC, int *includingFileC){
 
 	FILE * file				= NULL; //각 파일의 파일 포인터
-	char **includingFile 	= NULL; //리턴할 포함하고 있는 파일 목록
-	char * line 			= NULL; //각 파일의 행의 내용
-	int *checkEx 			= NULL; //and 옵션 체크용 배열, check existence;
+	char **includingFile 			= NULL; //리턴할 포함하고 있는 파일 목록
+	char * line 				= NULL; //각 파일의 행의 내용
+	int *checkEx 				= NULL; //and 옵션 체크용 배열, check existence;
 	int goal 				= 0;	//각 파일이 포함하고 있어야 할 키워드(검색 키워드)의 갯수
-	int sumOfGoal 			= 0;	//각 파일이 포함하고 있는 검색 키워드의 갯수  ex) 검색 키워드: google, facebook, 파일: Hello! google -> 루프후 sumofgoal: 1;
+	int sumOfGoal 				= 0;	//각 파일이 포함하고 있는 검색 키워드의 갯수  ex) 검색 키워드: google, facebook, 파일: Hello! google -> 루프후 sumofgoal: 1;
 	size_t len 				= 0;	//함수의 인수용 변수
 	ssize_t read;					//함수의 인수용 변수
 	
@@ -28,7 +28,8 @@ char **findFile(int option, char** keywordList, int keywordListC, char** fileNam
 
 
 	//serch including files
-	for (int j = 0; j < fileNameListC; j++){
+	for (int j = 0; j < fileNameListC; j++)
+	{
 		
 		file = fopen( fileNameList[j], "r");
 
@@ -47,7 +48,8 @@ char **findFile(int option, char** keywordList, int keywordListC, char** fileNam
 
 
 		//sumofgoal이 goal에 해당하면 including 파일에 저장 ex) 검색 키워드: facbook, google, and option: on, 파일 내용: Hello facebook -> 루프후 해당파일을 저장하지 않음, goal = 2이기. 
-        if (sumOfGoal >=  goal){	
+        	if (sumOfGoal >=  goal)
+		{	
 			
 			*includingFileC += 1;
 			
@@ -61,7 +63,7 @@ char **findFile(int option, char** keywordList, int keywordListC, char** fileNam
 		//모든 키워드가 포함 되어 있는지 체크
 		sumOfGoal = 0;
 		for (int i = 0; i < keywordListC; i++)	checkEx[i] = 0;
-        fclose(file);
+        	fclose(file);
 	}
 
 
